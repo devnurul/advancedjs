f4fdfc5eeae43ea4bdbfaf5f2d3a69c4522c1261
freelancernoor <info@devnurul.me>
2021-08-22 19:21:00 +0600

    true and false

---
+++ trueFalse.js
@@ -0,0 +1,19 @@
+//  const name = "";
+let name = NaN;
+
+ if(name){
+    console.log("condition is true");
+ }
+ else{
+     console.log("condition is false");
+ }
+
+//  truthy 
+// " ", 0, []
+
+// falsy
+//0
+//""
+//null
+//NaN
+
